9ebaf4942340a4bf8576f9f4a955af3f403fd85d
shu yusa <shu.yusa@gmail.com>
2018-04-29 17:32:46 +0900

    update modules

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,10 +3,12 @@ import path from 'path';
 import webpack from 'webpack';
 
 export default {
-  entry: './src/app.js',
+  entry: {
+    app: './src/app.js',
+  },
   output: {
     path: path.resolve(__dirname, 'js'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   resolve: {
     extensions: ['.js'],
@@ -17,11 +19,11 @@ export default {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      }
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      { test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/, use: [ 'url-loader' ] },
+      { test: /\.json$/, use: [ 'json-loader' ] }
     ],
   }
 };
