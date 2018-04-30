236c16850f32504dfc34d7ae0636aad833effaf4
shu yusa <shu.yusa@gmail.com>
2018-05-01 00:42:36 +0900

    add css loaders, jquery

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -17,6 +17,18 @@ export default {
       'node_modules'
     ]
   },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    // settings read from enviroment variables
+    new webpack.DefinePlugin({
+      'process.env.ENV': JSON.stringify(process.env.ENV),
+    }),
+  ],
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
